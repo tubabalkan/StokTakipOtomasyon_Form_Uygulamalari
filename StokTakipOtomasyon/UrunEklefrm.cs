f1ddd15c0e9af28e447c5f04923a5fcc0d7efa23
tubabalkan <tubabalkan165@gmail.com>
2023-02-28 00:16:35 +0300

    Update UrunEklefrm.cs
    veri tabanı bağlantısını ekledim

--- StokTakipOtomasyon/UrunEklefrm.cs
+++ StokTakipOtomasyon/UrunEklefrm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,6 +17,19 @@ namespace StokTakipOtomasyon
         {
             InitializeComponent();
         }
+        SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Stok_Takip;Integrated Security=True");
+
+        private void kategoriGetir()
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from kategoribilgileri", baglanti);
+            SqlDataReader read = komut.ExecuteReader();
+            while (read.Read())
+            {
+                comboKategori.Items.Add(read["kategori"].ToString());
+            }
+            baglanti.Close();
+        }
 
         private void btnIptal_Click(object sender, EventArgs e)
         {
@@ -24,6 +38,52 @@ namespace StokTakipOtomasyon
 
         private void UrunEklefrm_Load(object sender, EventArgs e)
         {
+            kategoriGetir();
+        }
+
+        private void comboKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboMarka.Items.Clear();
+            comboMarka.Text = "";
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from markabilgileri where kategori = '"+comboKategori.SelectedItem+"'", baglanti);
+            SqlDataReader read = komut.ExecuteReader();
+            while (read.Read())
+            {
+                comboMarka.Items.Add(read["marka"].ToString());
+            }
+            baglanti.Close();
+        }
+
+        private void btnYeniUrunEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into urun(barkodno, kategori,marka,urunadi,miktari,alisfiyati,satisfiyati,tarih) values(@barkodno, @kategori, @marka, @urunadi, @miktari, @alisfiyati, @satisfiyati, @tarih)", baglanti);
+            komut.Parameters.AddWithValue("@barkodno",txtBarkodNo.Text);
+            komut.Parameters.AddWithValue("@kategori", comboKategori.Text);
+            komut.Parameters.AddWithValue("@marka", comboMarka.Text);
+            komut.Parameters.AddWithValue("@urunadi", txtUrunAdi.Text);
+            komut.Parameters.AddWithValue("@miktari", int.Parse(txtMiktarı.Text));
+            komut.Parameters.AddWithValue("@alisfiyati",double.Parse(txtAlisFiyat.Text));
+            komut.Parameters.AddWithValue("@satisfiyati",double.Parse(txtSatisFiyat.Text));
+            komut.Parameters.AddWithValue("@tarih", DateTime.Now.ToString());
+         
+
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            MessageBox.Show("Ürün Başarılı Bir Şekilde Eklendi");
+            foreach (Control item in grpYeniUrun.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text = "";
+                }
+                if (item is ComboBox)
+                {
+                    item.Text = "";
+                }
+            }
+
 
         }
     }
