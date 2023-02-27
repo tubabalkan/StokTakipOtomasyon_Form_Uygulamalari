6582f0b8d429449c4c08c4ec7734792d8ede316f
tubabalkan <tubabalkan165@gmail.com>
2023-02-28 00:15:57 +0300

    Update Markafrm.cs
    Veri tabanı bağlantısı ekledim

--- StokTakipOtomasyon/Markafrm.cs
+++ StokTakipOtomasyon/Markafrm.cs
@@ -26,8 +26,31 @@ namespace StokTakipOtomasyon
 
         private void Markafrm_Load(object sender, EventArgs e)
         {
+            kategoriGetir();
+        }
 
+        private void kategoriGetir()
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from kategoribilgileri", baglanti);
+            SqlDataReader read = komut.ExecuteReader();
+            while (read.Read())
+            {
+                comboKategori.Items.Add(read["kategori"].ToString());
+            }
+            baglanti.Close();
+        }
 
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into markabilgileri(kategori, marka) values ('"+comboKategori.Text+"'   ,'" + txtMarka.Text + "')", baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            txtMarka.Text = "";
+            comboKategori.Text = "";
+
+            MessageBox.Show("Marka Eklendi");
         }
     }
 }
