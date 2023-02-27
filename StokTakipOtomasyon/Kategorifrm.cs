ec7f62e3436dccc5da2e570c7ea1bf9d3878a8cf
tubabalkan <tubabalkan165@gmail.com>
2023-02-28 00:15:01 +0300

    Update Kategorifrm.cs
    Veri tabanı bağlantısı ekledim

--- StokTakipOtomasyon/Kategorifrm.cs
+++ StokTakipOtomasyon/Kategorifrm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -16,13 +17,24 @@ namespace StokTakipOtomasyon
         {
             InitializeComponent();
         }
-
+        SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Stok_Takip;Integrated Security=True");
         private void btnIptal_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
         private void btnEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("insert into kategoribilgileri(kategori) values ('"+txtKategori.Text+"')", baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            txtKategori.Text = "";
+            MessageBox.Show("Kategori Eklendi");
+
+        }
+
+        private void Kategorifrm_Load(object sender, EventArgs e)
         {
 
         }
