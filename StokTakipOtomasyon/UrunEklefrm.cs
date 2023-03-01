804c6bfe2a0e596cf5e601659be5c4c142e6cedb
tubabalkan <tubabalkan165@gmail.com>
2023-03-01 13:25:53 +0300

    Update UrunEklefrm.cs
    barkodkontrol adında bir metot oluşturdum
    Daha sonra tabloyu datagridwiev kısmında görünmesini sağladım

--- StokTakipOtomasyon/UrunEklefrm.cs
+++ StokTakipOtomasyon/UrunEklefrm.cs
@@ -18,7 +18,22 @@ namespace StokTakipOtomasyon
             InitializeComponent();
         }
         SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Stok_Takip;Integrated Security=True");
-
+        bool durum;
+        private void barkodKontrol()
+        {
+            durum = true;
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from urun", baglanti);
+            SqlDataReader read = komut.ExecuteReader();
+            while (read.Read())
+            {
+                if (txtBarkodNo.Text == read["barkodno"].ToString() || txtBarkodNo.Text == "")
+                {
+                    durum = false;
+                }
+            }
+            baglanti.Close();
+        }
         private void kategoriGetir()
         {
             baglanti.Open();
@@ -57,7 +72,10 @@ namespace StokTakipOtomasyon
 
         private void btnYeniUrunEkle_Click(object sender, EventArgs e)
         {
-            baglanti.Open();
+            barkodKontrol();
+            if (durum==true)
+            {
+ baglanti.Open();
             SqlCommand komut = new SqlCommand("insert into urun(barkodno, kategori,marka,urunadi,miktari,alisfiyati,satisfiyati,tarih) values(@barkodno, @kategori, @marka, @urunadi, @miktari, @alisfiyati, @satisfiyati, @tarih)", baglanti);
             komut.Parameters.AddWithValue("@barkodno",txtBarkodNo.Text);
             komut.Parameters.AddWithValue("@kategori", comboKategori.Text);
@@ -72,6 +90,13 @@ namespace StokTakipOtomasyon
             komut.ExecuteNonQuery();
             baglanti.Close();
             MessageBox.Show("Ürün Başarılı Bir Şekilde Eklendi");
+            }
+            else
+            {
+                MessageBox.Show("Böyle bir barkod no var","Uyarı");
+            }
+           
+            comboMarka.Items.Clear();
             foreach (Control item in grpYeniUrun.Controls)
             {
                 if (item is TextBox)
@@ -85,6 +110,55 @@ namespace StokTakipOtomasyon
             }
 
 
+        }
+
+        private void grpYeniUrun_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BarkodNotxt_TextChanged(object sender, EventArgs e)
+        {
+            if (BarkodNotxt.Text == "")
+            {
+                lblMiktari.Text = "";
+                foreach (Control item in grpVarOlanUrun.Controls)
+                {
+                    if (item is TextBox)
+                    {
+                        item.Text = "";
+                    }
+                }
+            }
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("select * from urun where barkodno like '"+BarkodNotxt.Text+"'", baglanti);
+            SqlDataReader read = komut.ExecuteReader();
+            while (read.Read())
+            {
+                Kategoritxt.Text = read["kategori"].ToString();
+                Markatxt.Text = read["marka"].ToString();
+                UrunAditxt.Text = read["urunadi"].ToString();
+                Miktaritxt.Text = read["miktari"].ToString();
+                AlisFiyatitxt.Text = read["alisfiyati"].ToString();
+                SatisFiyatitxt.Text = read["satisfiyati"].ToString();
+            }
+            baglanti.Close();
+        }
+
+        private void btnVarOlanUrunEkle_Click(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut = new SqlCommand("update urun set miktari = miktari + '"+int.Parse(Miktaritxt.Text)+"' where barkodno = '"+BarkodNotxt.Text+"'", baglanti);
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            foreach (Control item in grpVarOlanUrun.Controls)
+            {
+                if (item is TextBox)
+                {
+                    item.Text = "";
+                }
+            }
+            MessageBox.Show("Var olan ürüne ekleme yapıldı");
         }
     }
 }
