8912ff8b08f9c38476c647aaf312fce24c506945
tubabalkan <tubabalkan165@gmail.com>
2023-03-02 18:16:42 +0300

    Update Markafrm.Designer.cs
    Formlarda Düzenleme Yaptım

--- StokTakipOtomasyon/Markafrm.Designer.cs
+++ StokTakipOtomasyon/Markafrm.Designer.cs
@@ -29,12 +29,15 @@ namespace StokTakipOtomasyon
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Markafrm));
             this.comboKategori = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.txtMarka = new System.Windows.Forms.TextBox();
             this.btnEkle = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
             this.SuspendLayout();
             // 
             // comboKategori
@@ -48,6 +51,7 @@ namespace StokTakipOtomasyon
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Silver;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.Location = new System.Drawing.Point(50, 42);
             this.label1.Name = "label1";
@@ -58,6 +62,7 @@ namespace StokTakipOtomasyon
             // label2
             // 
             this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Silver;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label2.Location = new System.Drawing.Point(62, 69);
             this.label2.Name = "label2";
@@ -74,32 +79,50 @@ namespace StokTakipOtomasyon
             // 
             // btnEkle
             // 
+            this.btnEkle.BackColor = System.Drawing.Color.Silver;
             this.btnEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.btnEkle.Location = new System.Drawing.Point(197, 94);
+            this.btnEkle.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnEkle.ImageIndex = 0;
+            this.btnEkle.ImageList = this.ımageList1;
+            this.btnEkle.Location = new System.Drawing.Point(197, 95);
             this.btnEkle.Name = "btnEkle";
-            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.Size = new System.Drawing.Size(75, 47);
             this.btnEkle.TabIndex = 4;
             this.btnEkle.Text = "Ekle";
-            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.btnEkle.UseVisualStyleBackColor = false;
             this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
-            this.button1.Location = new System.Drawing.Point(197, 123);
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.ImageIndex = 1;
+            this.button1.ImageList = this.ımageList1;
+            this.button1.Location = new System.Drawing.Point(116, 94);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(75, 48);
             this.button1.TabIndex = 5;
             this.button1.Text = "İptal";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "indir.jpg");
+            this.ımageList1.Images.SetKeyName(1, "indir (1).jpg");
             // 
             // Markafrm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.ClientSize = new System.Drawing.Size(367, 207);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(333, 182);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.btnEkle);
             this.Controls.Add(this.txtMarka);
@@ -123,5 +146,6 @@ namespace StokTakipOtomasyon
         private System.Windows.Forms.TextBox txtMarka;
         private System.Windows.Forms.Button btnEkle;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ImageList ımageList1;
     }
 }
