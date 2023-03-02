873ea3396275a5affd8a2f846560596f9f485439
tubabalkan <tubabalkan165@gmail.com>
2023-03-02 16:53:21 +0300

    Create satisListelefrm.cs
    veri tabanı bağlantısı ekledim. satışı yapılan ürünleri datagridviewde listeledim

---
+++ StokTakipOtomasyon/satisListelefrm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokTakipOtomasyon
+{
+    public partial class satisListelefrm : Form
+    {
+        public satisListelefrm()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglanti = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Stok_Takip;Integrated Security=True");
+        DataSet daset = new DataSet();
+
+
+        private void satislistele()
+        {
+            baglanti.Open();
+            SqlDataAdapter adtr = new SqlDataAdapter("select * from satis", baglanti);
+            adtr.Fill(daset, "satis");
+            dataGridView1.DataSource = daset.Tables["satis"];
+
+            baglanti.Close();
+
+        }
+        private void satisListelefrm_Load(object sender, EventArgs e)
+        {
+            satislistele();
+        }
+    }
+}
